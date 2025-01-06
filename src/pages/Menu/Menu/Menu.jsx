0550604ce2ce5cdb8fe7031b1e370cb2b24fcd81
designer-sujon <cdnl2018@gmail.com>
2025-01-06 13:02:58 +0600

    added custom hooks

--- src/pages/Menu/Menu/Menu.jsx
+++ src/pages/Menu/Menu/Menu.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import { Helmet } from "react-helmet-async";
 import Cover from "../../shared/Cover/Cover";
 import menuImg from "../../../assets/menu/banner3.jpg";
-import PopularMenu from "../../Home/PopularMenu/PopularMenu";
+
 
 const Menu = () => {
   return (
@@ -11,11 +11,7 @@ const Menu = () => {
         <title>Bistro | Menu</title>
       </Helmet>
       <Cover img={menuImg} title="Our Menu"></Cover>
-      <PopularMenu></PopularMenu>
-      <Cover img={menuImg} title="Our Menu"></Cover>
-      <PopularMenu></PopularMenu>
-      <Cover img={menuImg} title="Our Menu"></Cover>
-      <PopularMenu></PopularMenu>
+
     </div>
   );
 };
